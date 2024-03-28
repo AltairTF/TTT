cc2f9ea6a0417149bb2819a46241b906462ce6c2
AltairTF <todescatto6@gmail.com>
2024-03-28 20:48:33 -0300

    testando configs

--- src/views/Tabletop/index.jsx
+++ src/views/Tabletop/index.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 function Tabletop() {
+  const [tabletop, setTabletop] = useState([
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ]);
+
   return (
     <div>
-      <h1>Tabletop</h1>
+      <h1>Tic-Tac-Toe</h1>
+      <div>
+        {tabletop.map((row) => (
+          <div
+            style={{
+              heigth: "50px",
+              width: "50px",
+              backgroundColor: "red",
+              border: "1px solid blue",
+            }}
+          >
+            ALOU
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
